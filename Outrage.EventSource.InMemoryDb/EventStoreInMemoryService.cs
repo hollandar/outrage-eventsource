fc274191d1d9db5a192d3983fa811f690f5cb974
adrian holland <hollandar@gmail.com>
2023-01-18 11:30:23 +1100

    In memory testing event store

---
+++ Outrage.EventSource.InMemoryDb/EventStoreInMemoryService.cs
@@ -0,0 +1,43 @@
+﻿using Outrage.EventSource.Core;
+using System.Diagnostics;
+
+namespace Outrage.EventSource.InMemoryDb
+{
+    public class EventStoreInMemoryService : IEventStoreService
+    {
+        IDictionary<string, IList<EventContainer>> events;
+
+        public EventStoreInMemoryService()
+        {
+            this.events = new Dictionary<string, IList<EventContainer>>();
+        }
+
+        public Task<long> AppendToStreamAsync(string stream, string eventType, long version, string json)
+        {
+            IList<EventContainer> eventList;
+            if (!this.events.TryGetValue(stream, out eventList))
+            {
+                eventList = new List<EventContainer>();
+                this.events[stream] = eventList;
+            }
+
+            eventList.Add(new EventContainer(eventType, version, json, DateTimeOffset.UtcNow));
+
+            Debug.Assert(version == eventList.Count - 1);
+            return Task.FromResult(version);
+        }
+
+#pragma warning disable CS1998
+
+        public async IAsyncEnumerable<EventContainer> ReadStreamAsync(string streamName)
+        {
+            if (this.events.TryGetValue(streamName, out var eventList))
+            {
+                foreach (var eventContainer in eventList) yield return eventContainer;
+            }
+        }
+
+#pragma warning restore CS1998
+
+    }
+}
